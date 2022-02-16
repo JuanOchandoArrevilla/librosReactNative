8cd7544a066be282124d74de924efe854e25cdfc
Juan Ochando <usr2668@salesianos-lacuesta.net>
2022-02-16 11:48:31 +0000

    style: se crearon 2 botones

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { useState } from "react";
 
 export default function App() {
+
+  const [showModalLeer, setShowModalLeer] = useState(false);
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity style={styles.locateBotom1} onPress={() => }>  
+      <View style={styles.personalizarBotom}>
+        <Text >Libros que quiero leer</Text>     
+      </View>
+     </TouchableOpacity>
+
+     <TouchableOpacity style={styles.locateBotom}>  
+      <View style={styles.personalizarBotom}>
+      <Text >Libros que he leido</Text>
+      </View>
+     </TouchableOpacity>
+
     </View>
   );
 }
@@ -17,4 +30,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  personalizarBotom: {
+    backgroundColor: '#5856D6',
+    width: 70,
+    height: 40,
+  },
+  locateBotom: {
+    position: 'absolute',
+    bottom:10,
+    right:10
+  },
+  locateBotom1: {
+    position: 'absolute',
+    bottom:10,
+    right:100
+  }
 });
